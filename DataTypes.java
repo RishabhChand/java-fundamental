cf8bfa27c9cfe0cfdc931f3763f4a7146eb8aadc
Rishabh Chand <92012991+RishabhChand@users.noreply.github.com>
2021-10-12 13:59:08 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+package precourse_java;
+
+public class Datatypes {
+
+	public static void main(String args[]) {
+		byte my_age = 21;
+		short my_salary=10000;
+		int pincode = 497229;
+		long my_phone = 9981894800L;
+				
+		float percentage=94.5f;
+		double anuual_salary=2847284.2412752969d;
+		
+		boolean isIndian = true;
+		char Section='B';
+		
+		String my_name="Ujjwal";
+		
+		System.out.println(my_age);
+		System.out.println(my_salary);
+		System.out.println(pincode);
+		System.out.println(my_phone);
+		System.out.println(percentage);
+		System.out.println(anuual_salary);
+		System.out.println(isIndian);
+		System.out.println(Section);
+		System.out.println(my_name);
+		
+	}
+}
---
+++ ifelse.java
@@ -0,0 +1,26 @@
+package precourse_java;
+
+public class ifelse {
+
+	public static void main(String args[]) {
+		int num1=100;
+		int num2=200;
+		
+		if (num1==num2) {
+			System.out.println("equal");
+		}
+		else {
+			System.out.println("Not equal");
+		}
+		
+		System.out.println(100==200);
+		System.out.println(200==200);
+		System.out.println(100>2);
+		System.out.println(100<20);
+		System.out.println(100<100);
+		System.out.println(100<=1000);
+		System.out.println(1000>=200);
+		System.out.println(100!=200);
+		System.out.println(100!=100);
+	}
+}
